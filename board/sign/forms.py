9fa6afee007784804f6efd1a513b08d34a94b088
Lebedeva316 <lebedeva316@gmail.com>
2022-07-06 22:57:37 +0300

    Init Skillfactory

---
+++ board/sign/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name','username','email','avatar','password1','password2']
+
